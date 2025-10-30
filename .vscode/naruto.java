4a5228a154ad829278dbcee3de688dec9740a6c4
HALLEY2001 <jarod.menatutiven@gmail.com>
2025-10-30 10:15:38 +0100

    Inicio

---
+++ .vscode/naruto.java
@@ -0,0 +1,17 @@
+public class naruto {
+
+    public static void main(String[] args) {
+
+    String usuario = "";
+    String contraseña = "";
+
+    System.out.println("Introduce tu usuario y la contraseña:");
+
+    if (usuario.equalsIgnoreCase("Hokage") && contraseña.equalsIgnoreCase("contraseña")) {
+        System.out.println("Hola muy buenas");
+    } else {
+        System.out.println("prueba de nuevo");
+    }
+//Esto es una prueba.
+    }
+}
